2ef2c71342ee907f86c79aafd79ef2907092271c
dohyeonan <dh.an@4grit.com>
2024-11-25 22:56:25 +0900

    feat: set db_models_structure

---
+++ app/src/user/infra/repository/user_repo.py
@@ -0,0 +1,35 @@
+from src.utils.database.temp_database import SessionLocal
+from src.user.domain.repository.user_repo import IUserRepository
+from src.user.domain.user import User as UserVO
+from src.user.infra.db_models.user import User
+from fastapi import HTTPException
+from src.utils.db_utils import row_to_dict
+
+
+class UserRepository(IUserRepository):
+    def save(self, user:UserVO):
+        new_user = User(
+            id=user.id,
+            email=user.email,
+            name=user.name,
+            password=user.password,
+            created_at=user.created_at,
+            updated_at=user.updated_at,
+        )
+
+        with SessionLocal as db:
+            try:
+                db = SessionLocal()
+                db.add(new_user)
+                db.commit()
+            finally:
+                db.close()
+
+    def find_by_email(self, email:str) -> UserVO:
+        with SessionLocal() as db:
+            user = db.query(User).filter(User.email == email).first()
+
+        if not user:
+            raise HTTPException(status_code=422)
+        
+        return UserVO(**row_to_dict(user))
