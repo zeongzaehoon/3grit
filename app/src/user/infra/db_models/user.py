2f3786e9a889a46edd037ae6638a23846dcf7ed8
dohyeonan <dh.an@4grit.com>
2025-02-16 01:02:53 +0900

    86erf75zn: autoincrement-pk-in-user-model

--- app/src/user/infra/db_models/user.py
+++ app/src/user/infra/db_models/user.py
@@ -1,12 +1,12 @@
 from datetime import datetime
-from sqlalchemy import Column, String, DateTime
+from sqlalchemy import Column, String, DateTime, Integer, Identity
 from sqlalchemy.orm import Mapped, mapped_column
 from utils.database import Base
 
 class User(Base):
     __tablename__ = "user"
 
-    id: Mapped[str] = mapped_column(String(36), primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, Identity(), primary_key=True)
     name: Mapped[str] = mapped_column(String(32), nullable=False)
     email: Mapped[str] = mapped_column(String(64), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String(64), nullable=False)
